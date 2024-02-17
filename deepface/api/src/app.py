43f464f9315c5afc761933742dff0b3e97d28f56
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2024-02-17 13:32:04 -0800

    Add CORS support to Flask app

--- deepface/api/src/app.py
+++ deepface/api/src/app.py
@@ -1,8 +1,12 @@
 # 3rd parth dependencies
 from flask import Flask
 from deepface.api.src.modules.core.routes import blueprint
+from cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(blueprint)
     return app
+
